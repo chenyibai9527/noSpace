3c404cffe00fd6978686d26319485080f75ab92c
Chenyibai <107304274+chenyibai9527@users.noreply.github.com>
2023-08-09 14:57:47 +0800

    update

--- script.js
+++ script.js
@@ -1,107 +1,131 @@
 // 多语言支持
 const languages = {
-    en: {
-      title: "Remove Spaces",
-      inputCharacterCountLabel: "Input Characters",
-      outputCharacterCountLabel: "Output Characters",
-      removeSpacesButton: "Remove Spaces",
-      copyButton: "Copy",
-      placeholder:"Input something...",
-    },
-    zh: {
-      title: "去除空格",
-      inputCharacterCountLabel: "输入字符数",
-      outputCharacterCountLabel: "输出字符数",
-      removeSpacesButton: "去除空格",
-      copyButton: "复制",
-      placeholder:"输入内容...",
-    },
-    ja: {
-      title: "スペースの削除",
-      inputCharacterCountLabel: "入力文字数",
-      outputCharacterCountLabel: "出力文字数",
-      removeSpacesButton: "スペース除去",
-      copyButton: "コピー",
-      placeholder:"入力...",
-    },
-    ko: {
-        title: "공백 제거 도구",
-        inputCharacterCountLabel: "입력 문자 수",
-        outputCharacterCountLabel: "출력 문자 수",
-        removeSpacesButton: "공백 제거",
-        copyButton: "복사",
-        placeholder: "텍스트를 입력하세요..."
-    },
-    nl: {
-        title: "Verwijder spaties",
-        inputCharacterCountLabel: "Invoertekens",
-        outputCharacterCountLabel: "Uitvoertekens",
-        removeSpacesButton: "Verwijder spaties",
-        copyButton: "Kopiëren",
-        placeholder: "Voer iets in...",
-    },
-    de: {
-        title: "Leerzeichen entfernen",
-        inputCharacterCountLabel: "Eingabezeichen",
-        outputCharacterCountLabel: "Ausgabezeichen",
-        removeSpacesButton: "Leerzeichen entfernen",
-        copyButton: "Kopieren",
-        placeholder: "Etwas eingeben...",
-    },
-    fr: {
-        title: "Supprimer les espaces",
-        inputCharacterCountLabel: "Caractères saisis",
-        outputCharacterCountLabel: "Caractères en sortie",
-        removeSpacesButton: "Supprimer les espaces",
-        copyButton: "Copier",
-        placeholder: "Saisissez quelque chose...",
-    },
-    zh_FT: {
-        title: "移除空格",
-        inputCharacterCountLabel: "輸入字元",
-        outputCharacterCountLabel: "輸出字元",
-        removeSpacesButton: "移除空格",
-        copyButton: "複製",
-        placeholder: "輸入內容...",
-    },
-    ru: {
-        title: "Удалить пробелы",
-        inputCharacterCountLabel: "Количество символов ввода",
-        outputCharacterCountLabel: "Количество символов вывода",
-        removeSpacesButton: "Удалить пробелы",
-        copyButton: "Копировать",
-        placeholder: "Введите что-нибудь...",
-    },
-    it: {
-        title: "Rimuovi spazi",
-        inputCharacterCountLabel: "Caratteri di input",
-        outputCharacterCountLabel: "Caratteri di output",
-        removeSpacesButton: "Rimuovi spazi",
-        copyButton: "Copia",
-        placeholder: "Inserisci qualcosa...",
-    },
-    es: {
-        title: "Eliminar espacios",
-        inputCharacterCountLabel: "Caracteres de entrada",
-        outputCharacterCountLabel: "Caracteres de salida",
-        removeSpacesButton: "Eliminar espacios",
-        copyButton: "Copiar",
-        placeholder: "Ingrese algo...",
-    },
-  };
-  
-  function setLanguage(language) {
-    const languageData = languages[language];
-  
-    // 设置标题和按钮文本
-    document.getElementById("title").textContent = languageData.title;
-    document.getElementById("inputCharacterCountLabel").textContent = languageData.inputCharacterCountLabel;
-    document.getElementById("outputCharacterCountLabel").textContent = languageData.outputCharacterCountLabel;
-    document.getElementById("removeSpacesButton").textContent = languageData.removeSpacesButton;
-    document.getElementById("copyButton").textContent = languageData.copyButton;
-    const inputTextElement = document.getElementById("inputText");
-    inputTextElement.placeholder = languageData.placeholder;
-  }
+  en: {
+    title: "Remove Spaces",
+    inputCharacterCountLabel: "Input Characters",
+    outputCharacterCountLabel: "Output Characters",
+    removeSpacesButton: "Remove Spaces",
+    copyButton: "Copy",
+    placeholder:"Input something...",
+    toIndex:"Home",
+    toTools:"Tools",
+  },
+  zh: {
+    title: "删除空格",
+    inputCharacterCountLabel: "输入字符数",
+    outputCharacterCountLabel: "输出字符数",
+    removeSpacesButton: "删除空格",
+    copyButton: "复制",
+    placeholder:"输入内容...",
+    toIndex:"首页",
+    toTools:"工具",
+  },
+  ja: {
+    title: "スペースを削除する",
+    inputCharacterCountLabel: "入力文字数",
+    outputCharacterCountLabel: "出力文字数",
+    removeSpacesButton: "スペース除去",
+    copyButton: "コピー",
+    placeholder:"入力...",
+    toIndex:"ホームページ",
+    toTools:"ツール",
+  },
+  ko: {
+      title: "공백 제거",
+      inputCharacterCountLabel: "입력 문자 수",
+      outputCharacterCountLabel: "출력 문자 수",
+      removeSpacesButton: "공백 제거",
+      copyButton: "복사",
+      placeholder: "텍스트를 입력하세요...",
+      toIndex:"홈",
+      toTools:"도구",
+  },
+  nl: {
+      title: "Verwijder spaties",
+      inputCharacterCountLabel: "Invoertekens",
+      outputCharacterCountLabel: "Uitvoertekens",
+      removeSpacesButton: "Verwijder spaties",
+      copyButton: "Kopiëren",
+      placeholder: "Voer iets in...",
+      toIndex:"Startpagina",
+      toTools:"Gereedschap",
+  },
+  de: {
+      title: "Leerzeichen entfernen",
+      inputCharacterCountLabel: "Eingabezeichen",
+      outputCharacterCountLabel: "Ausgabezeichen",
+      removeSpacesButton: "Leerzeichen entfernen",
+      copyButton: "Kopieren",
+      placeholder: "Etwas eingeben...",
+      toIndex:"Startseite",
+      toTools:"Werkzeuge",
+  },
+  fr: {
+      title: "Supprimer les espaces",
+      inputCharacterCountLabel: "Caractères saisis",
+      outputCharacterCountLabel: "Caractères en sortie",
+      removeSpacesButton: "Supprimer les espaces",
+      copyButton: "Copier",
+      placeholder: "Saisissez quelque chose...",
+      toIndex:"Page d'accueil",
+      toTools:"Outils",
+  },
+  zh_FT: {
+      title: "刪除空格",
+      inputCharacterCountLabel: "輸入字元",
+      outputCharacterCountLabel: "輸出字元",
+      removeSpacesButton: "刪除空格",
+      copyButton: "複製",
+      placeholder: "輸入內容...",
+      toIndex:"首頁",
+      toTools:"工具",
+  },
+  ru: {
+      title: "Удалить пробелы",
+      inputCharacterCountLabel: "Количество символов ввода",
+      outputCharacterCountLabel: "Количество символов вывода",
+      removeSpacesButton: "Удалить пробелы",
+      copyButton: "Копировать",
+      placeholder: "Введите что-нибудь...",
+      toIndex:"Главная страница",
+      toTools:"Инструменты",
+  },
+  it: {
+      title: "Rimuovi spazi",
+      inputCharacterCountLabel: "Caratteri di input",
+      outputCharacterCountLabel: "Caratteri di output",
+      removeSpacesButton: "Rimuovi spazi",
+      copyButton: "Copia",
+      placeholder: "Inserisci qualcosa...",
+      toIndex:"Pagina iniziale",
+      toTools:"Strumenti",
+  },
+  es: {
+      title: "Eliminar espacios",
+      inputCharacterCountLabel: "Caracteres de entrada",
+      outputCharacterCountLabel: "Caracteres de salida",
+      removeSpacesButton: "Eliminar espacios",
+      copyButton: "Copiar",
+      placeholder: "Ingrese algo...",
+      toIndex:"Página de inicio",
+      toTools:"Herramientas",
+  },
+};
+
+function setLanguage(language) {
+  const languageData = languages[language];
+
+  // 设置标题和按钮文本
+  document.getElementById("title").textContent = languageData.title;
+  document.getElementById("inputCharacterCountLabel").textContent = languageData.inputCharacterCountLabel;
+  document.getElementById("outputCharacterCountLabel").textContent = languageData.outputCharacterCountLabel;
+  document.getElementById("removeSpacesButton").textContent = languageData.removeSpacesButton;
+  document.getElementById("copyButton").textContent = languageData.copyButton;
+  document.getElementById("toIndex").textContent = languageData.toIndex;
+  document.getElementById("toTools").textContent = languageData.toTools;
+  const inputTextElement = document.getElementById("inputText");
+  inputTextElement.placeholder = languageData.placeholder;
+}
   
   function changeLanguage(language) {
     setLanguage(language);
@@ -131,3 +155,14 @@ const languages = {
     outputText.setSelectionRange(0, 99999);
     document.execCommand("copy");
   }
+
+  function removeExtraSpaces() {
+    // 获取输入的文本
+    var inputText = document.getElementById("inputText").value;
+
+    // 删除多余空格
+    var processedText = inputText.replace(/\s+/g, " ");
+
+    // 显示处理后的文本
+    document.getElementById("outputText").innerHTML = processedText;
+}
